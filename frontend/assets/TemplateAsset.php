3f6b89f810150714fa831d7f0520620117e13967
muxroka2525 <95241995+muxroka2525@users.noreply.github.com>
2022-04-07 11:19:40 +0500

    git pull

---
+++ frontend/assets/TemplateAsset.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class TemplateAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+        'css/bootstrap.min.css',
+        'css/style.css',
+        'css/versions.css',
+        'css/responsive.css',
+        'css/custom.css'
+    ];
+    public $js = [
+        'js/modernizer.js',
+        'js/all.js',
+        'js/custom.js',
+        'js/timeline.min.js'
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap4\BootstrapAsset',
+    ];
+}
